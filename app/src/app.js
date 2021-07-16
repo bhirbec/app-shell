2b430fbc87e4d41b8f546717c3cf8f431b04a68c
Benoit Hirbec <benoit@squareup.com>
2021-07-16 15:32:15 -0600

    Clean up some CSS

--- app/src/app.js
+++ app/src/app.js
@@ -1,23 +1,21 @@
 import { Layout, Menu } from 'antd';
 import 'antd/dist/antd.css';
+import './index.css';
 
 const { Header, Content, Footer } = Layout;
 
 function App() {
   return (
-    <Layout className="layout" style={{ minHeight: "100vh" }}>
-      <Header style={{ position: "sticky", top: "0" }}>
-        <div className="logo" />
-        <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['2']}>
-          <Menu.Item>Nav 1</Menu.Item>
+    <Layout className="layout">
+      <Header>
+        <Menu theme="dark" mode="horizontal">
+          <Menu.Item key="1">Nav 1</Menu.Item>
         </Menu>
       </Header>
-      <Content style={{ padding: '25px 50px' }}>
-        <div className="site-layout-content">
-          <div>Content</div>
-        </div>
+      <Content>
+        <div>Content</div>
       </Content>
-      <Footer style={{ position: "sticky", bottom: "0", textAlign: 'center', backgroundColor: '#e3e3e3' }}>
+      <Footer>
         Retro Tool ©2021
       </Footer>
     </Layout>
