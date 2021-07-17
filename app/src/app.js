f54beacd714c54d581227c62de58abd4670db087
Benoit Hirbec <benoit@squareup.com>
2021-07-17 01:20:52 -0600

    Add basic react router

--- app/src/app.js
+++ app/src/app.js
@@ -1,4 +1,11 @@
 import { Layout, Menu } from 'antd';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import {Signin, Signout} from './signin';
 
 import 'antd/dist/antd.css';
@@ -9,21 +16,40 @@ const { Header, Content, Footer } = Layout;
 function App(props) {
   return (
     <Layout className="layout">
-      <Header>
-        {props.user !== null && (
-          <Menu theme="dark" mode="horizontal">
-            <Menu.Item key="1">Nav</Menu.Item>
-            <Menu.Item key="2"><Signout /></Menu.Item>
-          </Menu>
-        )}
-      </Header>
-      <Content>
-        {props.user === null ?
-          <Signin />
-          :
-          <div>Content</div>
-        }
-      </Content>
+      <Router>
+        <Header>
+          {props.user !== null && (
+            <Menu theme="dark" mode="horizontal">
+              <Menu.Item key="1">
+                <Link to="/">Home</Link>
+              </Menu.Item>
+              <Menu.Item key="2">
+                <Link to="/about">About</Link>
+              </Menu.Item>
+              <Menu.Item key="3"><Signout /></Menu.Item>
+            </Menu>
+          )}
+        </Header>
+        <Content>
+          {props.user === null && (
+            <Signin />
+          )}
+
+          {props.user !== null && (
+            <Switch>
+              <Route path="/signin">
+                <Signin />
+              </Route>
+              <Route path="/about">
+                About
+              </Route>
+              <Route path="/">
+                Home
+              </Route>
+            </Switch>
+          )}
+        </Content>
+      </Router>
       <Footer>
         Retro Tool ©2021
       </Footer>
